7fef15d7d53d296ac51ac6bcdd87ca7ac753b738
John Mark Centeno <jeiem.swdev@gmail.com>
2024-07-17 22:07:46 +0800

    feat: added necessary interfaces and classes for CQRS pattern using MediatR

---
+++ RunningTracker.Application/Abstractions/Messaging/IQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using RunningTracker.Infrastructure.Shared;
+
+namespace RunningTracker.Application.Abstractions.Messaging
+{
+    public interface IQuery<TResponse> : IRequest<Result<TResponse>>
+    {
+    }
+}
---
+++ RunningTracker.Application/Abstractions/Messaging/IQueryHandler.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using RunningTracker.Infrastructure.Shared;
+
+namespace RunningTracker.Application.Abstractions.Messaging
+{
+    public interface IQueryHandler<TQuery, TResponse> : IRequestHandler<TQuery, Result<TResponse>>
+        where TQuery : IQuery<TResponse>
+    {
+    }
+}
